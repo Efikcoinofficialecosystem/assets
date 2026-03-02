84991c8f98ed30139b5fd2f6b2d6417f4f493cb5
Efikcoin official ecosyste <Efikcoinofficialecosystem@gmail.com>
2026-03-02 10:46:53 +0100

    Efikcoin Eternal ($ECE) logo

---
+++ .github/workflows/blockchain/smartwallet.ts
@@ -0,0 +1,14 @@
+This PR adds the official logo for EfikCoin Eternal (ECE) on Binance Smart Chain.
+
+Token Details:
+- Contract: 0x9f8c29e496ecb6c39c221458f211234dfcb233e0 (checksum format)
+- Name: EfikCoin Eternal
+- Symbol: ECE
+- Decimals: 18
+- Blockchain: BSC (smartchain)
+
+Project Links:
+- Website: https://efikcoinofficialecosystem.github.io/Efikcoin.ecosystem/
+- Explorer: https://bscscan.com/address/0x9f8c29e496ecb6c39c221458f211234dfcb233e0
+
+Logo: 256x256 PNG, white background, under 100kB, named logo.png
